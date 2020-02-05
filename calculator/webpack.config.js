1267bb5e4dccd736ad56779ce4becb31e85817bd
wlukla <vladislav.danilochkin@gmail.com>
2020-02-05 20:41:17 +0200

    feat: create default project structure

---
+++ calculator/webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    })
+  ]
+}
+
+module.exports = config;
